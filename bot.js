b1c27080f1811ce1e9549b1510c66a81121947e8
Ouadie Lahdioui <ouadiesoft@gmail.com>
2018-07-31 18:50:16 +0200

    Adding bot.js and skills examples

--- bot.js
+++ bot.js
@@ -13,39 +13,40 @@ This is a sample Google Hangouts Chat bot built with Botkit.
     -> https://developers.google.com/hangouts/chat/how-tos/bots-publish
 
   Run your bot from the command line:
-    DEBUG=botkit:*
+    DEBUG=botkit:* \
     PORT=YOUR_APP_PORT \
     GOOGLE_APPLICATION_CREDENTIALS=YOUR_GOOGLE_CREDENTIALS_FILE \
     GOOGLE_VERIFICATION_TOKEN=YOUR_GOOGLE_VERIFICATION_TOKEN \
-     node bot.js
+    node bot.js
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-var env = require('node-env-file');
-env(__dirname + '/.env');
+const env = require('node-env-file');
 
+env(__dirname + '/.env');
 
 if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
     console.log('Error: Specify a GOOGLE_APPLICATION_CREDENTIALS in environment.');
     process.exit(1);
 }
 
-var Botkit = require('botkit');
-var debug = require('debug')('botkit:main');
+const Botkit = require('botkit');
 
-// Create the Botkit controller, which controls all instances of the bot.
-var controller = Botkit.googlehangoutsbot({
-    endpoint: 'Axjn86rTGRQwisaYFyT0XZyiOCh7rZUPGx1A',
-    token: process.env.GOOGLE_VERIFICATION_TOKEN,
-    port: process.env.PORT,
-    debug: true,
+const endpoint = "Axjn86rTGRQwisaYFyT0XZyiOCh7rZUPGx1A";
+const token = process.env.GOOGLE_VERIFICATION_TOKEN;
+const port = process.env.PORT;
+const debug = true;
+
+const controller = Botkit.googlehangoutsbot({
+    endpoint,
+    token,
+    port,
+    debug,
 });
 
-var bot = controller.spawn({});
+const bot = controller.spawn({});
+const webserver = require(__dirname + '/components/express_webserver.js')(controller, bot);
 
-// Set up an Express-powered webserver to expose oauth and webhook endpoints
-var webserver = require(__dirname + '/components/express_webserver.js')(controller, bot);
-
-var normalizedPath = require("path").join(__dirname, "skills");
+const normalizedPath = require("path").join(__dirname, "skills");
 require("fs").readdirSync(normalizedPath).forEach(function(file) {
     require("./skills/" + file)(controller);
 });
--- components/express_webserver.js
+++ components/express_webserver.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var debug = require('debug')('botkit:webserver');
+const express = require('express');
+const bodyParser = require('body-parser');
+const debug = require('debug')('botkit:webserver');
 
-module.exports = function(controller, bot) {
+module.exports = (controller, bot) => {
 
-
-    var webserver = express();
+    let webserver = express();
     webserver.use(bodyParser.json());
     webserver.use(bodyParser.urlencoded({extended: true}));
 
     // import express middlewares that are present in /components/express_middleware
-    var normalizedPath = require("path").join(__dirname, "express_middleware");
+    let normalizedPath = require("path").join(__dirname, "express_middleware");
 
-    require("fs").readdirSync(normalizedPath).forEach(function (file) {
+    require("fs")
+        .readdirSync(normalizedPath)
+        .forEach( (file) => {
         require("./express_middleware/" + file)(webserver, controller);
     });
 
@@ -20,12 +21,11 @@ module.exports = function(controller, bot) {
 
     webserver.listen(process.env.PORT || 3000);
 
-    controller.createWebhookEndpoints(webserver, bot, function () {
+    controller.createWebhookEndpoints(webserver, bot,  () => {
         debug(`🚀 Congratulation, the web server is online!`);
     });
 
     controller.webserver = webserver;
 
     return webserver;
-
 }
